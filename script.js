f273fd6f48ae5e6d13ab868920a4ec118e378c8f
Gabriel Catani <gabrielm.catani@gmail.com>
2023-09-15 11:13:06 -0300

    update: add Lexer to generate token of keys pressed.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+let nbr1 = undefined;
+let nbr2 = undefined;
+let operator = undefined;
+
 function add(nbr1, nbr2) {
     return nbr1 + nbr2;
 }
@@ -16,7 +20,7 @@ function divide(nbr1, nbr2) {
     return nbr1 / nbr2;
 }
 
-function commandParser(keyPressed) {
+function commandLexer(keyPressed) {
     if (keyPressed === 'zeroKey') {
         return 0;
     }
@@ -29,12 +33,36 @@ function commandParser(keyPressed) {
     return keyPressed;
 }
 
-const calculator = document.querySelector(".calculator");
+function commandParser(token) {
+    if (typeof token === 'number') {
+        if (!nbr1) {
+            nbr1 = token;
+        }
+        else {
+            nbr2 = token;
+        }
+    }
+    else if (token !== 'execute' &&
+             token !== 'comma') {
+        operator = token;
+    }
+    else if (token === 'execute') {
+        console.log("EXECUTE CALC");
+    }
+    console.log("Nbr1: " + nbr1 + "\nNbr2: " + nbr2 + "\nOp: " + operator);
+}
 
-const buttons = Array.from(calculator.children);
+function calculatorInit() {
+    const calculator = document.querySelector(".calculator");
 
-buttons.forEach((calcKey) => {
-    calcKey.addEventListener('click', () => {
-        console.log(commandParser(calcKey.getAttribute('id')));
+    const buttons = Array.from(calculator.children);
+
+    buttons.forEach((calcKey) => {
+        calcKey.addEventListener('click', () => {
+            const token = commandLexer(calcKey.getAttribute('id'));
+            commandParser(token);
+        });
     });
-});
+}
+
+calculatorInit();
